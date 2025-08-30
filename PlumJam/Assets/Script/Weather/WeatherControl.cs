55e71ec925eae9cc03178a2bdc0f0e125f97c481
IAmNew <gm042568@gmail.com>
2025-08-31 02:26:35 +0900

    물 수위 상승

--- PlumJam/Assets/Script/Weather/WeatherControl.cs
+++ PlumJam/Assets/Script/Weather/WeatherControl.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using UnityEngine;
 
+public enum WeatherType
+{
+    LightRain,
+    HeavyRain,
+}
+
 public class WeatherManager : MonoBehaviour
 {
+    public static WeatherManager instance { get; private set; }
+
     [Header("Rain Settings")]
     [SerializeField] private ParticleSystem lightRainParticle;
     [SerializeField] private ParticleSystem heavyRainParticle;
     [SerializeField] private float heavyRainDuration;            //폭우 시간
     [SerializeField] private float heavyRainInterval;            //폭우 간격
 
+    public WeatherType weatherType = WeatherType.LightRain; //현재 날씨
+
     private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
         StartCoroutine(RainSequence());
     }
 
@@ -29,11 +48,15 @@ public class WeatherManager : MonoBehaviour
     {
         lightRainParticle.Play();
         heavyRainParticle.Stop();
+
+        weatherType = WeatherType.LightRain;
     }
 
     private void HeavyRain()
     {
         lightRainParticle.Stop();
         heavyRainParticle.Play();
+
+        weatherType = WeatherType.HeavyRain;
     }
 }
